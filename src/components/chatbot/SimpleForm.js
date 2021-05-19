a01e1406a72d2e4b02fb9850471b943b660bbef1
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 03:30:20 -0700

    rendered react simple chatbot on index.html via App.js

--- src/components/chatbot/SimpleForm.js
+++ src/components/chatbot/SimpleForm.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import ChatBot from 'react-simple-chatbot';
+
 
 class SimpleForm extends Component{
   render(){
     return(
-      <div>
+      <ChatBot
+        steps = {[
+          {
+            id: 'intro',
+            message: 'Hello world. I am a chatbot.',
+            end: true,
+          },
+        ]}
 
-      </div>
+      />
+
     );
   }
 
