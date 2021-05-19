c645bf669684a856f8b1aa55f99d2eb7fb88c346
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 04:21:08 -0700

    Added theme provider

--- src/components/chatbot/SimpleForm.js
+++ src/components/chatbot/SimpleForm.js
@@ -1,115 +1,130 @@
 import React, {Component} from 'react';
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#EF6C00',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#EF6C00',
+  botFontColor:  '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+}
 
 
 class SimpleForm extends Component{
   render(){
     return(
-      <ChatBot
-        steps = {[
-          {
-            id: 'intro',
-            message: 'Do you agree to Terms and Condition?',
-            trigger: 'intro-user',
-          },
-          {
-            id: 'intro-user',
-            options:[
-              {value:'y', label:'yes', trigger:'yes-response'},
-              {value: 'n', label:'No', trigger:'no-response'},
-            ]
-          },
-          {
-            id: 'yes-response',
-            message:'Great!',
-            trigger: 'q-name',
-          },
-          {
-            id: 'no-response',
-            message: 'Sorry to hear that.',
-            end: true,
-          },
-          {
-            id:'q-name',
-            message:'What is your name?',
-            trigger:'name',
-          },
-          {
-            id:'name',
-            user:true,
-            validator:(value) =>{
-              if(/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/.test(value))
-              {
-                return true;
-              }
-              else
-              {
-                return "Please input alphabet characters only.";
-              }
+      <ThemeProvider theme={theme}>
+        <ChatBot
+          steps = {[
+            {
+              id: 'intro',
+              message: 'Do you agree to Terms and Condition?',
+              trigger: 'intro-user',
             },
-            trigger: 'q-email',
-          },
-          {
-            id: "q-email",
-            message: 'What is your email?',
-            trigger:"email",
-          },
-          {
-            id: 'email',
-            user: true,
-            validator:(value) =>{
-              if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
-              {
-                return true;
-              }
-              else {
-                return 'Please enter a valid email.';
-              }
+            {
+              id: 'intro-user',
+              options:[
+                {value:'y', label:'yes', trigger:'yes-response'},
+                {value: 'n', label:'No', trigger:'no-response'},
+              ]
             },
-            trigger:'q-zip',
-          },
-          {
-            id: 'q-zip',
-            message: 'What is your zip code?',
-            trigger: 'zip',
+            {
+              id: 'yes-response',
+              message:'Great!',
+              trigger: 'q-name',
+            },
+            {
+              id: 'no-response',
+              message: 'Sorry to hear that.',
+              end: true,
+            },
+            {
+              id:'q-name',
+              message:'What is your name?',
+              trigger:'name',
+            },
+            {
+              id:'name',
+              user:true,
+              validator:(value) =>{
+                if(/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/.test(value))
+                {
+                  return true;
+                }
+                else
+                {
+                  return "Please input alphabet characters only.";
+                }
+              },
+              trigger: 'q-email',
+            },
+            {
+              id: "q-email",
+              message: 'What is your email?',
+              trigger:"email",
+            },
+            {
+              id: 'email',
+              user: true,
+              validator:(value) =>{
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
+                {
+                  return true;
+                }
+                else {
+                  return 'Please enter a valid email.';
+                }
+              },
+              trigger:'q-zip',
+            },
+            {
+              id: 'q-zip',
+              message: 'What is your zip code?',
+              trigger: 'zip',
 
-          },
-          {
-            id: 'zip',
-            user: true,
-            validator: (value) => {
-              if (/^[0-9]{5}(?:-[0-9]{4})?$/.test(value))
-              {
-                return true;
-              }
-              else
-              {
-                return "Please enter a valid zip code."
-              }
             },
-            trigger: 'q-phone'
-          },
-          {
-            id: 'q-phone',
-            message: "What is your phone number?",
-            trigger: 'phone',
-          },
-          {
-            id: 'phone',
-            user:true,
-            validator: (value) =>{
-              if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(value))
-              {
-                return true;
-              }
-              else {
-                return "Please enter a valid phone number.";
-              }
+            {
+              id: 'zip',
+              user: true,
+              validator: (value) => {
+                if (/^[0-9]{5}(?:-[0-9]{4})?$/.test(value))
+                {
+                  return true;
+                }
+                else
+                {
+                  return "Please enter a valid zip code."
+                }
+              },
+              trigger: 'q-phone'
             },
-            end:true,
-          },
-        ]}
-      />
+            {
+              id: 'q-phone',
+              message: "What is your phone number?",
+              trigger: 'phone',
+            },
+            {
+              id: 'phone',
+              user:true,
+              validator: (value) =>{
+                if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(value))
+                {
+                  return true;
+                }
+                else {
+                  return "Please enter a valid phone number.";
+                }
+              },
+              end:true,
+            },
+          ]}
+        />
+      </ThemeProvider>
     );
   }
 
