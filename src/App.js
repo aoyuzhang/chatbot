832f674fb772638da83a111e771a71f852c1aff7
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 03:25:51 -0700

    installed react-simple-chatbot and styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import SimpleForm from "./components/chatbot/SimpleForm";
 
 class App extends Component{
   render(){
     return(
       <div className = "App">
-        <h1>Some Text</h1>
+        <SimpleForm />
       </div>
     );
   }
---
+++ src/components/chatbot/SimpleForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SimpleForm extends Component{
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+
+}
+export default SimpleForm;
