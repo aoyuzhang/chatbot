d2b4080e614f6c6dcc9ba2edc31acd3c3877bfea
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 04:15:05 -0700

    added name, email, zip, phone number validator

--- src/components/chatbot/SimpleForm.js
+++ src/components/chatbot/SimpleForm.js
@@ -22,12 +22,91 @@ class SimpleForm extends Component{
           {
             id: 'yes-response',
             message:'Great!',
-            end: true,
+            trigger: 'q-name',
           },
           {
             id: 'no-response',
             message: 'Sorry to hear that.',
             end: true,
+          },
+          {
+            id:'q-name',
+            message:'What is your name?',
+            trigger:'name',
+          },
+          {
+            id:'name',
+            user:true,
+            validator:(value) =>{
+              if(/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/.test(value))
+              {
+                return true;
+              }
+              else
+              {
+                return "Please input alphabet characters only.";
+              }
+            },
+            trigger: 'q-email',
+          },
+          {
+            id: "q-email",
+            message: 'What is your email?',
+            trigger:"email",
+          },
+          {
+            id: 'email',
+            user: true,
+            validator:(value) =>{
+              if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
+              {
+                return true;
+              }
+              else {
+                return 'Please enter a valid email.';
+              }
+            },
+            trigger:'q-zip',
+          },
+          {
+            id: 'q-zip',
+            message: 'What is your zip code?',
+            trigger: 'zip',
+
+          },
+          {
+            id: 'zip',
+            user: true,
+            validator: (value) => {
+              if (/^[0-9]{5}(?:-[0-9]{4})?$/.test(value))
+              {
+                return true;
+              }
+              else
+              {
+                return "Please enter a valid zip code."
+              }
+            },
+            trigger: 'q-phone'
+          },
+          {
+            id: 'q-phone',
+            message: "What is your phone number?",
+            trigger: 'phone',
+          },
+          {
+            id: 'phone',
+            user:true,
+            validator: (value) =>{
+              if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(value))
+              {
+                return true;
+              }
+              else {
+                return "Please enter a valid phone number.";
+              }
+            },
+            end:true,
           },
         ]}
       />
